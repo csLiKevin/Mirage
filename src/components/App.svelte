efd75a413b1170ec48c0050ed6164066c71c2882
csLiKevin <csLiKevin@users.noreply.github.com>
2020-12-22 21:14:23 -0500

    Delete everything. Start transition to videojs.

--- src/components/App.svelte
+++
@@ -1,8 +0,0 @@
-<script context="module" type="text/javascript">
-    import FileDropArea from "./FileDropArea.svelte";
-    import Player from "./Player.svelte";
-</script>
-
-<FileDropArea let:filePath>
-    <Player {filePath} />
-</FileDropArea>
