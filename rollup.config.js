dc35e35f99ce2d678148ad0ccd6aaa0e32d6acf2
csLiKevin <csLiKevin@users.noreply.github.com>
2021-07-25 20:58:15 -0400

    Add @rollup/plugin-typescript as a dependency.

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import commonjs from "@rollup/plugin-commonjs";
 import postcss from "rollup-plugin-postcss";
 import resolve from "@rollup/plugin-node-resolve";
+import typescript from "@rollup/plugin-typescript";
 
 export default {
     input: "index.ts",
@@ -13,5 +14,6 @@ export default {
             browser: true,
         }),
         postcss(),
+        typescript(),
     ],
 };
