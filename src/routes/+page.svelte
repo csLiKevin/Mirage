14bd17b409c47a55d57f8cbaa58cc11fdd3744c1
csLiKevin <csLiKevin@users.noreply.github.com>
2023-09-12 10:10:31 -0400

    Optimize event handling.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -16,59 +16,57 @@
         vr: (options?: { [key: string]: string }) => VRVideoJsPlayer;
     }
 
+    let player: VRVideoJsPlayer;
+
+    function handleDrop(event: DragEvent) {
+        const { dataTransfer } = event;
+        const { files } = dataTransfer!;
+
+        if (!files.length) {
+            return;
+        }
+
+        const file = files[0];
+        player.src({ type: file.type, src: URL.createObjectURL(file) });
+    }
+
+    function handleKeyDown(event: KeyboardEvent) {
+        const { code } = event;
+
+        if (code === "KeyR") {
+            event.preventDefault();
+            const {
+                camera: { position },
+            } = player.vr();
+            position.x = 0;
+            position.y = 0;
+            position.z = 0;
+        }
+
+        if (code === "Space") {
+            event.preventDefault();
+            player.paused() ? player.play() : player.pause();
+        }
+    }
+
     onMount(async () => {
         const videojs = await import("video.js");
         await import("videojs-vr");
 
-        const player = videojs.default("video") as VRVideoJsPlayer;
+        player = videojs.default("video") as VRVideoJsPlayer;
         player.vr({ projection: "180_LR" });
 
         // Prevent videos from starting automatically if the play button was clicked before the video was loaded.
         player.addClass("vjs-show-big-play-button-on-pause");
         player.on("loadedmetadata", player.pause);
-
-        const containerElement = document.getElementById("container")!;
-
-        containerElement.addEventListener("dragover", (event) => {
-            event.preventDefault();
-        });
-
-        containerElement.addEventListener("drop", (event: DragEvent) => {
-            event.preventDefault();
-            const { dataTransfer } = event;
-            const { files } = dataTransfer!;
-
-            if (!files.length) {
-                return;
-            }
-
-            const file = files[0];
-            player.src({ type: file.type, src: URL.createObjectURL(file) });
-        });
-
-        document.addEventListener("keydown", (event) => {
-            const { code } = event;
-
-            if (code === "Space") {
-                event.preventDefault();
-                player.paused() ? player.play() : player.pause();
-                return;
-            }
-
-            if (code === "KeyR") {
-                event.preventDefault();
-                const {
-                    camera: { position },
-                } = player.vr();
-                position.x = 0;
-                position.y = 0;
-                position.z = 0;
-                return;
-            }
-        });
     });
 </script>
 
+<svelte:body
+    on:dragover|preventDefault
+    on:drop|preventDefault={handleDrop}
+    on:keydown={handleKeyDown}
+/>
 <div id="container">
     <!-- svelte-ignore a11y-media-has-caption -->
     <video id="video" class="video-js" controls preload="auto" />
